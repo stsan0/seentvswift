6b3fb10e546e155b3c48669756f56e5f65b1f4e7
stsan0 <54484048+stsan0@users.noreply.github.com>
2023-04-07 00:06:31 -0700

    starting project

---
+++ seentv/seentv/seentv/seentvApp.swift
@@ -0,0 +1,17 @@
+//
+//  seentvApp.swift
+//  seentv
+//
+//  Created by Steven Tsan on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct seentvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
